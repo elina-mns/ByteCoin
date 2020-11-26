7b38f944502090eb13b41d28f09199241439b61b
elina-mns <ellie.mansurova@gmail.com>
2020-11-26 17:46:57 -0500

    perform request and parse Jason data

--- Model/CoinManager.swift
+++ Model/CoinManager.swift
@@ -11,11 +11,42 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "1AEA1EE2-2CF7-4886-A047-4DF5F0A3E3FA"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    
     func getCoinPrice(for currency: String) {
-        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                let dataString = String(data: data!, encoding: .utf8)
+                print(dataString)
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJason(_ exchangeData: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(ExchangeData.self, from: exchangeData)
+            let lastPrice = decodedData.rate
+            return lastPrice
+        } catch {
+            print(error)
+            return nil
+        }
     }
 }
+
+
---
+++ Model/CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Elina Mansurova on 2020-11-26.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct ExchangeData: Codable {
+
+    let rate: Double
+}
